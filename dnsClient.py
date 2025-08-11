f290cb46cced1ae16f81df65eef599b1d3cd2aaa
nziccardi <nsz2023@nyu.edu>
2025-08-10 22:04:30 -0400

    Update dnsClient.py

--- dnsClient.py
+++ dnsClient.py
@@ -81,8 +81,8 @@ def dns_query(type, name, server):
     # 2. nyu (length of 3)
     # 3. edu (length of 3)
 
-    qname_parts = name.split('????')  # How can we easily split the string?
-    qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('????') for part in
+    qname_parts = name.split('.')  # How can we easily split the string?
+    qname_encoded_parts = [struct.pack('B', len(part)) + part.encode('ascii') for part in
                            qname_parts]  # Make sure it's encoded as a sequence of the right character encoding type (lowercase)
     qname_encoded = b''.join(
         qname_encoded_parts) + b'\x00'  # enter the closing byte value to signify the end of the domain string (two digits)
